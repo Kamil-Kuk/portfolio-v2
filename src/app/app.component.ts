b5bef5e5fbb50f7bf54de957f2096910c430f06b
kamilK <kamil.kukowski@gmail.com>
2021-05-19 22:37:12 +0200

    fix[lang] added polish version and fuctional language switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-v2';
+  translations;
+
+  constructor(private translateService: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.translations = this.translateService.store.translations[`${this.translateService.defaultLang}`];
+  }
+
+  public changeLangPol(): void {
+    this.translateService.setDefaultLang('pol');
+  }
+
+  public changeLangEng(): void {
+    this.translateService.setDefaultLang('eng');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { WorkComponent } from './component/work/work.component';
 import { ProjectComponent } from './component/project/project.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { ResumeComponent } from './component/resume/resume.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
+import { TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function translateHttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -20,9 +26,24 @@ import { ContactComponent } from './component/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translateHttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  availableLng = ['eng', 'pol'];
+
+  constructor(private translateService: TranslateService) {
+    const tmpLng = 'eng';
+    translateService.setDefaultLang(tmpLng);
+  }
+
+}
